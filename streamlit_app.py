7c505e2fee863cdfe8bd32a33040eabecb837b21
Kharaderahul <95746230+Kharaderahul@users.noreply.github.com>
2023-05-25 15:49:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,13 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry oatmeal')
 streamlit.text('Kale, Spinach & Rocket smoothie') 
 streamlit.text('Hard-Boiled Free-Range egg')
+
+ streamlit.title('My Mom's New Healthy Diner')
+  
+streamlit.header('Breakfast Favourites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
+                 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
